e8932028b25dee855866d13ccdab8df14e47eb28
Kristian Thaulow <kristian1990@gmail.com>
2019-07-04 18:39:42 +0200

    Fixed user login routing

--- WebShopClient/src/app/layout/header/header.component.ts
+++ WebShopClient/src/app/layout/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  private currentUser: User;
 
-  constructor() { }
+  constructor(private router: Router,
+    private authenticationService: AuthenticationService) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.authenticationService.logout();
   }
 
 }
