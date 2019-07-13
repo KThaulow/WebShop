7cf16931d9853b1a0b8f18fefdb7f27179e5d582
Kristian Thaulow <kristian1990@gmail.com>
2019-07-13 11:29:59 +0200

    Moved secrets to local secrets file

--- WebShopServer/WebShop/Startup.cs
+++ WebShopServer/WebShop/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -61,5 +62,7 @@ namespace WebShop
 			app.UseHttpsRedirection();
 			app.UseMvc();
 		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
 	}
 }
