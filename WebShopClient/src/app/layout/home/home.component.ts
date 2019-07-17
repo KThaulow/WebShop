ca49b757bf162d8d70f340f5330962d96791a7a9
Kristian Thaulow <kristian1990@gmail.com>
2019-07-17 19:09:01 +0200

    Moved image url to reference in TS

--- WebShopClient/src/app/layout/home/home.component.ts
+++ WebShopClient/src/app/layout/home/home.component.ts
@@ -9,11 +9,15 @@ import { User } from 'src/app/models/user';
 })
 export class HomeComponent implements OnInit {
   private currentUser: User;
+  private background1: string;
+  private background2: string;
 
   constructor(private authenticationService: AuthenticationService) {
     this.currentUser = this.authenticationService.currentUserValue;
   }
 
   ngOnInit() {
+    this.background1 = "/assets/nature.jpg";
+    this.background2 = "/assets/thailand.jpg";
   }
 }
